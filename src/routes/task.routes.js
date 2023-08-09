2143ea8c229de40e3bbadead7196309efe18c00f
Jhostym <you@example.com>
2023-08-09 17:42:37 -0500

    probando git

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -4,7 +4,7 @@ import { authRequired } from "../middelwares/validateToken.js";
 const router = Router();
 
 router.get("/tasks", authRequired ,(req, res) => {
-  res.send("tasks");
+  res.send("tasks con exito");
 });
 
 export default router;
