4c5605f6eff5a75c82b63bdb4cf098ca1930781a
Jhostym <you@example.com>
2023-08-10 17:26:16 -0500

    agregando validaciones

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 import { register, login, logout, profile } from '../controllers/auth.controller.js';
 import { authRequired } from '../middelwares/validateToken.js';
+import { validateSchema } from '../middelwares/validate.auth.js';
+import { registerSchema,loginSchema } from '../schemas/auth.schema.js';
 
 const router = Router();
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', validateSchema(registerSchema)  ,register);
+router.post('/login', validateSchema(loginSchema), login);
 router.post('/logout', logout);
 router.get('/profile', authRequired, profile);
 
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,11 +1,26 @@
 import { Router } from "express";
+import {
+  createTask,
+  deleteTask,
+  getTask,
+  getTasks,
+  updateTask,
+} from "../controllers/task.controller.js";
 import { authRequired } from "../middelwares/validateToken.js";
+import { validateSchema } from "../middelwares/validate.auth.js";
+import { createTaskSchema } from "../schemas/task.schema.js";
+
 
 const router = Router();
 
-router.get("/tasks", authRequired ,(req, res) => {
-  res.send("tasks con exito");
-});
+router.get("/tasks", authRequired, getTasks);
+
+router.post("/tasks", authRequired, validateSchema(createTaskSchema), createTask);
+
+router.get("/tasks/:id", authRequired, getTask);
+
+router.put("/tasks/:id", authRequired, updateTask);
+
+router.delete("/tasks/:id", authRequired, deleteTask);
 
 export default router;
-
